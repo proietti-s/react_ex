d61f6f8aa5533a612f63fed6d1c4fe97137f94da
proietti-s <stefano.pro@docsity.com>
2022-01-25 12:39:33 +0100

    add dataCustomers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+const DATA_CUSTOMERS = [
+  {id: 'c1', firstName: 'Paul', secName: 'Dumars', email:'paul@mail.it'},
+  {id: 'c2', firstName: 'John', secName: 'Mieli', email:'mieli@mail.it'},
+  {id: 'c3', firstName: 'Sandra', secName: 'Fieldman', email:'sandra@mail.it'},
+  {id: 'c4', firstName: 'Lola', secName: 'Aiuola', email:'lola@mail.it'},
+  {id: 'c5', firstName: 'Billy', secName: 'Ballo', email:'billy@mail.it'}
+]
+
 function App() {
   
 
