d8981901ed25158760b38c9284492bb7732bca30
proietti-s <stefano.pro@docsity.com>
2022-01-25 16:10:04 +0100

    ripristino esercizio

--- src/components/Customers/CustomersList.js
+++ src/components/Customers/CustomersList.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Card from "../UI/Card";
 
-<<<<<<< HEAD
 const CustomersList = (props) => {
   /* const customers = props.data.map(customer => console.log(customer)) */
   return (
@@ -25,29 +24,5 @@ const CustomersList = (props) => {
     </>
   );
 };
-=======
 
-const CustomersList = props => {
-    // const [customer, setCustomer] = useState('')
-    // const customers = props.data.map((customer) => console.log(customer))
-
-    return (
-        <>
-            {props.data.map((customer) =>
-                <Card key={customer.id}>
-                    <header>
-                        <h2>Customer</h2>
-                    </header>
-                    <div>
-                        <p><b>Name</b>: {customer.firstName} </p>
-                        <p><b>LastName</b>: {customer.secName} </p>
-                        <p><b>Email</b>: {customer.email} </p>
-                    </div>
-                </Card>
-            )}
-        </>
-    )
-}
->>>>>>> 9b1edac0af4209434a1b6bc1a611808118cbd46a
-
-export default CustomersList
+export default CustomersList;
