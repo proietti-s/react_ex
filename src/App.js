4d756dd23a9bc3c65ad51a0e26f907c228874ee8
proietti-s <stefano.pro@docsity.com>
2022-01-25 12:34:48 +0100

    new branch customersEx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
-
-import AddUser from "./components/Users/AddUser";
-import UserList from "./components/Users/UserList";
+import React from "react";
 
 function App() {
-  const [user, setUser] = useState({ name: "", age: "" });
-
-  const saveUser = (formEvent) => {
-    if (formEvent)
-      return setUser({
-        name: formEvent.target.username.value.toString(),
-        age: formEvent.target.age.value.toString(),
-      });
-  };
+  
 
   return (
     <div>
-      <AddUser onSave={saveUser} />
-      <UserList name={user.name} age={user.age} />
+    
     </div>
   );
 }
 
 export default App;
-
-//commento per commit
